8d17f3fcc8cc0d7374e6df82db53cd37bff9d264
murayama-taichi <24j5140@stu.meisei-u.ac.jp>
2025-01-09 03:17:51 +0900

    Update kentei.py

--- kentei.py
+++ kentei.py
@@ -3,7 +3,11 @@ p_0 = 1/3
 def func():
     bunshi = x-n*p_0
     bunbo = math.sqrt(n*p_0*(1-p_0))
-    print(bunshi/bunbo)
+    print(bunshi/bunbo,end=" ")
+    if bunshi/bunbo >= 1.64:
+        print("H_0")
+    else:
+        print("H_1")
 
 #全体
 x = 182
